a1683dfc33b6b1bf14848a4c99db62e5698b3918
Leon.King <106268294@qq.com>
2021-03-14 22:09:09 +0800

    1day

---
+++ src/main/java/com/southwind/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.southwind.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.southwind.mybatisplus.entity.account;
+
+public interface UserMapper extends BaseMapper<account> {
+
+}
